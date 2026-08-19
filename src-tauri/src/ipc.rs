f018300ef4b8d7e442475e3d4e6aee50ee004fa8
心衍 <vier@j7yx.com>
2026-08-19 18:50:24 +0800

    feat: use simple_tauri utils for npm latest ver, switch dep back to path

--- src-tauri/src/ipc.rs
+++ src-tauri/src/ipc.rs
@@ -1,4 +1,19 @@
+
 #[tauri::command]
 pub fn ipc_version() -> String {
     env!("CARGO_PKG_VERSION").to_string()
+}
+
+#[tauri::command]
+pub fn ipc_server_version() -> String {
+    "0.1.0-rc.6".to_string()
+}
+
+#[tauri::command]
+pub fn ipc_server_latest_ver() -> String {
+    simple_tauri::utils::get_npm_latest_ver("@deepseek-ai/dsh").to_string()
+}
+
+#[tauri::command]
+pub fn ipc_server_update() {
 }
