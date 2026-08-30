5dd819aed7e2771174ba4250dcf800083f075205
心衍 <vier@j7yx.com>
2026-08-31 00:42:57 +0800

    清理 scripts 目录

--- scripts/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "PATH=;%PATH%"
-rem pnpm install @deepseek-ai/dsh
-rem pnpm approve-builds -all
-rem echo @echo off > start.cmd
-rem echo set "DSH_HOME=%~dp0.dsh" >> start.cmd
-rem echo. >> start.cmd
-rem echo ./node_modules/.bin/dsh.CMD web --no-open --port 34333 --trusted-host 127.0.0.1 >> start.cmd
-rem echo. >> start.cmd
