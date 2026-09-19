7b125117d82ed2db0e77a6be0f64083b0c88d8af
心衍 <vier@j7yx.com>
2026-09-20 00:33:04 +0800

    refactor: use show_env_cmd from library

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 use simple_tauri::simple_tray;
 use simple_tauri::simple_serve;
 use simple_tauri::utils::sh2rs::sh2rs;
+use indoc::indoc;
 
 mod ipc;
 mod config;
@@ -20,4 +21,24 @@ fn show_setting() {
     simple_tray::show_window("main");
     sh2rs!("sleep 1").ok();
     simple_tray::runjs("main", r#"document.querySelector('[data-slot="sidebar.settings"]>button').click()"#);
+}
+
+fn show_cmd() {
+    let server_home   = config::get_or!("server_home","");
+    #[cfg(windows)]
+    simple_tauri::utils::show_env_cmd(&format!(indoc! {r#"
+            set "DSH_HOME={}"
+            set "PATH=%DSH_HOME%/node_modules/.bin;%PATH%"
+            cls
+            cd ~
+            dsh --help
+        "#},server_home));
+    #[cfg(not(windows))]
+    simple_tauri::utils::show_env_cmd(&format!(indoc! {r#"
+            export DSH_HOME="{}"
+            export PATH="$DSH_HOME/node_modules/.bin:$PATH"
+            clear
+            cd ~
+            dsh --help
+        "#},server_home));
 }
